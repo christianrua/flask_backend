f88391d292c4458111554557f910b6eaa0afb890
Christian Rua <christian.rua@autodesk.com>
2022-07-05 16:25:55 -0500

    first commit

---
+++ backend.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+import os, platform
+ 
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+
+    host_name = ""
+
+    if platform.system() == "Windows":
+        host_name = platform.uname().node
+    else:
+        host_name = os.uname()[1]  # doesnt work on windows
+    
+    return f"<p>you are seeing the response from {host_name}</p>"
+
+if __name__ == '__main__':
+   app.run()    
