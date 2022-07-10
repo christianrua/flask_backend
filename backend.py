8ced2bf6d642a340df8212d9b5921dba456de9c4
christian.rua@gmail.com <christian.rua@gmail.com>
2022-07-09 19:41:22 -0500

    adding s3 access

--- backend.py
+++ backend.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 
-import os, platform
+import os, platform, datetime
+
+import boto3
+s3_client = boto3.client('s3')
  
 app = Flask(__name__)
 
@@ -15,8 +18,18 @@ def hello_world():
         host_name = os.uname()[1]  # doesnt work on windows
 
     ip_addr = request.remote_addr    
+    file_name = f'log_{ip_addr}_{datetime.now()}.txt'
+
+    with open(file_name, 'w') as f:
+        f.write(f'{ip_addr} 200 {datetime.now()}')
+
+    response = s3_client.put_object(
+    Body=file_name,
+    Bucket='sysopsadmin-christian',
+    Key='flask-logs',
+)
     
-    return f"<p>you are seeing the response from {host_name}, and your IP is {ip_addr}</p>"
+    return f"<p>you are seeing the response from {host_name}, and your IP is {ip_addr}, s3 response {response}</p>"
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=5000)    
