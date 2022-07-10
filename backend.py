fb9ffaf566677e10b03cd8a09430587703132723
christian.rua@gmail.com <christian.rua@gmail.com>
2022-07-09 19:29:20 -0500

    adding the client ip

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 import os, platform
  
@@ -13,8 +13,10 @@ def hello_world():
         host_name = platform.uname().node
     else:
         host_name = os.uname()[1]  # doesnt work on windows
+
+    ip_addr = request.remote_addr    
     
-    return f"<p>you are seeing the response from {host_name}</p>"
+    return f"<p>you are seeing the response from {host_name}, and your IP is {ip_addr}</p>"
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=5000)    
